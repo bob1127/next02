4c20b3361803e8dee164fb212a058a7b962983ec
bob1127 <61307295+bob1127@users.noreply.github.com>
2023-08-21 09:51:45 +0800

    third commit

---
+++ app/(default)/about.jsx
@@ -0,0 +1,31 @@
+export const metadata = {
+    title: 'ABOUT - Simple',
+    description: 'Page description',
+}
+
+// import Hero from '@/components/hero'
+// import Features from '@/components/features'
+// import FeaturesBlocks from '@/components/features-blocks'
+// import Testimonials from '@/components/testimonials'
+// import Newsletter from '@/components/newsletter'
+// import CardHover from '../../components/CardHover'
+// import ApplicateCard from '../../components/ApplicateCard'
+
+
+
+export default function Home() {
+    return (
+        <>
+            {/* <Hero />
+            <CardHover />
+            <ApplicateCard />
+
+
+            <Features />
+            <FeaturesBlocks />
+            <Testimonials />
+            <Newsletter /> */}
+            <h2>ijijijijijijijij</h2>
+        </>
+    )
+}
